95e4d87ff2f7efa90d446537953cb03bf4e6c9da
venketeshq <venketesh@yahoo.com>
2025-12-29 15:48:30 -0500

    reat-api-v1

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/health", methods=["GET"])
 def health():
     return jsonify({"status": "OK"})
+
+# GET endpoint
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({
+        "message": "Hello from Python server"
+    })
+
+# POST endpoint
+@app.route('/data', methods=['POST'])
+def receive_data():
+    payload = request.json
+    return jsonify({
+        "status": "received",
+        "data": payload
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
